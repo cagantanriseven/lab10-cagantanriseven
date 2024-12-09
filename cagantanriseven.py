01e038df16efc10a220b231b95c28fdbf62b4843
cagantanriseven <cagan.tanriseven@std.yeditepe.edu.tr>
2024-12-09 17:21:53 +0300

    Add cagantanriseven.py

---
+++ cagantanriseven.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    i = 1
+    result = 1
+    if n==0:
+        result = 1
+    else:
+        while i<n:
+            result = result*(i+1)
+            i = i+1
+    return result    
+if factorial(3)==6:
+    print("No errors.")    
